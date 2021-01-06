8fbedd1865baab3ab7013a1954f80195cc2f3d0d
Max Sayer <sayer_max@yahoo.co.uk>
2021-01-06 11:16:21 +0000

    Help and Tier commands

--- main.py
+++ main.py
@@ -35,10 +35,74 @@ bot = commands.Bot(command_prefix=config["prefix"], help_command=None)
 bot.add_listener(on_ready)
 
 
+# functions
+async def image_paths_to_bytes(paths):
+    images = []
+
+    for f in paths:
+        images.append(Image.open(os.path.join(os.path.dirname(__file__), f)))
+
+    combined_image = Image.new("RGBA", (len(paths) * images[0].width, images[0].height))
+    
+    for count, image in enumerate(images):
+        combined_image.paste(image, (count * image.width, 0))
+
+    combined_image = combined_image.resize((combined_image.width // 2, combined_image.height // 2))
+
+    byte = io.BytesIO()
+    combined_image.save(byte, format="PNG")
+    byte.seek(0)
+
+    return byte
+
 # commands
+@bot.command()
+async def help(ctx):
+    text = ("`>names` lists the names of civilisations\n"
+            "`>tier [1-6]` lists the civilisations in a certain Filthy Robot tier\n"
+            "`>[tiers|tierlist]` shows the entire Filthy Robot tier list\n"
+            "`>draft` starts a civilisation draft\n"
+            "`>draft [no_players] [no_civs_per_player]` quick draft command without bans")
+
+    await ctx.send("**Help**")
+    await ctx.send(text)
+
+
 @bot.command()
 async def names(ctx):
     await ctx.send("\n".join([civ["name"] for civ in civilizations["civs"]]))
+
+
+@bot.command()
+async def tier(ctx, number):
+    number = int(number)
+
+    if (number > 6 or number < 1):
+        return
+    
+    civs_in_tier = []
+
+    for civ in civilizations["civs"]:
+        if civ["tier"] == number:
+            civs_in_tier.append(civ)
+    
+    flag_paths = ["images/flags/" + c["picture"] for c in civs_in_tier]
+    names = [c["name"] for c in civs_in_tier]
+    image_bytes = await image_paths_to_bytes(flag_paths)
+
+    await ctx.send(f"**Tier {number}**")
+    await ctx.send(", ".join(names))
+    await ctx.send(file=discord.File(image_bytes, filename="tier.png"))
+
+
+@bot.command(aliases=["tierlist"])
+async def tiers(ctx):
+    await tier(ctx, 1)
+    await tier(ctx, 2)
+    await tier(ctx, 3)
+    await tier(ctx, 4)
+    await tier(ctx, 5)
+    await tier(ctx, 6)
 
 
 @bot.command()
@@ -96,31 +160,15 @@ async def draft(ctx, no_players=None, no_civs_per_player=None):
 
     for i in range(no_players):
         options = random.sample(civ_choices, no_civs_per_player)
-        flag_paths = [o["picture"] for o in options]
+        flag_paths = ["images/flags/" + o["picture"] for o in options]
         names = [o["name"] for o in options]
         civ_choices = [c for c in civ_choices if c not in options]
 
-        await ctx.send(f"**Player {i+1}:**")
+        image_bytes = await image_paths_to_bytes(flag_paths)
+
+        await ctx.send(f"**Player {i+1}**")
         await ctx.send(", ".join(names))
-
-        # flag image
-        flag_images = []
-
-        for f in flag_paths:
-            flag_images.append(Image.open(os.path.join(os.path.dirname(__file__), "images/flags/" + f)))
-
-        combined_image = Image.new("RGBA", (len(flag_paths) * flag_images[0].width, flag_images[0].height))
-        
-        for count, image in enumerate(flag_images):
-            combined_image.paste(image, (count * image.width, 0))
-
-        combined_image = combined_image.resize((combined_image.width // 2, combined_image.height // 2))
-
-        byte = io.BytesIO()
-        combined_image.save(byte, format="PNG")
-        byte.seek(0)
-
-        await ctx.send(file=discord.File(byte, filename="draft.png"))
+        await ctx.send(file=discord.File(image_bytes, filename="draft.png"))
 
 
 
